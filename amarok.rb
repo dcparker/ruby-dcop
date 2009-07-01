610e9bf994fbea72e5b95e4648053ea2e2008502
Daniel Parker <dcparker@gmail.com>
2009-07-01 10:58:28 -0400

    Auto-generating Amarok library from dcop. Skeleton started.

--- amarok.rb
+++ amarok.rb
@@ -1,3 +1,11 @@
-class Amarok
-  
-end
+require 'dcop'
+# Automatically builds the Amarok library based on everything available in amarok through dcop!
+DCOP.build!('amarok', 'user' => 'jukeman', 'session' => '.DCOPserver_jukeman-elkhart__0')
+
+# Begin the process!
+# 1) Download the updates
+  journals = Journal.from_server
+# 2) Apply them to the database
+  journals.each {|j| j.apply }
+# 3) Apply database to Amarok
+  Playlist.active.apply_to_amarok
